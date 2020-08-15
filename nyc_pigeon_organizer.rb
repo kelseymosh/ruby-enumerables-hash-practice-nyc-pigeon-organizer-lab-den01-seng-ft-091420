69a91a6d80c0b223ba6c05683f1d0f0b297ea7df
Kelsey Oshiro <kelseymosh@gmail.com>
2020-08-15 21:31:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
   #binding.pry
   value.each do |inner_key,names|
-    names.each do |names|
+    names.each do |name|
       binding.pry
       end
     end
