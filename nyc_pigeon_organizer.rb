9af288eec2b9888b7bea8d870200eb2dd375068a
Kelsey Oshiro <kelseymosh@gmail.com>
2020-08-15 21:40:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,14 +8,14 @@ def nyc_pigeon_organizer(data)
       #binding.pry
       if !final_array[name]
         final_array[name] = {}
-        end
+          end
       if !final_array[name][key]
         !final_array[name][key] = []
-      end
+        end
       final_array[name][key].push(inner_key)
+      end
+      final_array
     end
-  final_array
   end
-end
   binding.pry
 end
